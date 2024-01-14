b8bb80917d8ebbcf920349ba47bab0364c0f05cb
robertnovell <120330156+robertnovell@users.noreply.github.com>
2024-01-13 20:19:41 -0500

    Initial Commit

---
+++ Que_personaje/Que_personajeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Que_personajeApp.swift
+//  Que_personaje
+//
+//  Created by UVEG on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Que_personajeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
